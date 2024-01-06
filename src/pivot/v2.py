784c9023e546518f0f7bf8a13537bb298d6a6d39
Lilith <__lilith@discord>
2024-01-06 16:19:41 +1100

    add better type hinting to v2

--- src/pivot/v2.py
+++ src/pivot/v2.py
@@ -6,6 +6,7 @@ from src.pivot.dtypes import ServerClasses, PlayerEphemeralConsts, UsefulServerC
 from pathlib import Path
 from loguru import logger
 from copy import deepcopy
+from typing import Any
 
 
 class DataCollectorV2:
@@ -83,24 +84,28 @@ class DataCollectorV2:
 
     def _get_mapped_accounts_by_time_period(self) -> None:
         logger.info("[V2] Extracting account details and life-times...")
-        _slots = {}
-        _account_ident_to_slot_id = {}
-        _user_ident_to_slot_id = {}
+        _slots: dict[int, PlayerEphemeralConsts] = {}
+        _account_ident_to_slot_id: dict[int, int] = {}
+        _user_ident_to_slot_id: dict[int, int] = {}
         # Extract the identifiers that map each of the slot names ("001", "002", ..., "099", "100")
         # to their m_iAccountID and m_iUserID variables
         logger.debug("[V2] Extracting identifiers...")
+        packet: dict[str, Any]
         for packet in self.demo_json_raw:
             if packet['type'] != 'DataTables':
                 continue
 
+            table: dict[str, Any]
             for table in packet['tables']:
                 if table['name'] == 'm_iAccountID':
+                    prop: dict[str, Any]
                     for prop in table['props']:
                         if prop['name'] not in _slots:
                             _slots[int(prop['name'])] = PlayerEphemeralConsts(prop['name'])
                         _slots[int(prop['name'])].m_iAccountID_identifier = int(prop['identifier'])
 
                 elif table['name'] == 'm_iUserID':
+                    prop: dict[str, Any]
                     for prop in table['props']:
                         if int(prop['name']) not in _slots:
                             _slots[int(prop['name'])] = PlayerEphemeralConsts(prop['name'])
@@ -108,6 +113,7 @@ class DataCollectorV2:
         # pivot on the slots data to create an efficient lookups for mapping identifier numbers to potential slot
         # ids. The keys will be the m_iUserID identifier or the m_iAccountID identifier (depending on var),
         # and the value will be the slot id that maps to (for grabbing the PlayerEphemeralConsts object from _slots)
+        slot_id: int
         for slot_id in _slots:
             # May as well initialise the dicts for later while we are here
             self.slot_details_by_tick_range[slot_id] = {}
@@ -123,16 +129,21 @@ class DataCollectorV2:
         #   Every time there is a change mapped to m_iUserID, there will be a corresponding change to m_iAccountID in
         #   the same packet/tick.
         logger.debug("[V2] Extracting values by time dependency...")
+        packet: dict[str, Any]
         for packet in self.demo_json_raw:
             if packet['type'] != "Message":
                 continue
 
             # I'm sorry for this, dear god.
+            # It actually turned out better than expected - iterating big JSON just tends
+            # to require multiple nested for loops
             tick_num = int(packet['tick'])
+            message: dict[str, Any]
             for message in packet['messages']:
                 if message['type'] != 'PacketEntities':
                     continue
 
+                entity: dict[str, Any]
                 for entity in message['entities']:
                     if not self.server_classes.is_id_named(
                             entity['server_class'],
@@ -140,9 +151,10 @@ class DataCollectorV2:
                     ):
                         continue
 
+                    prop: dict[str, Any]
                     for prop in entity['props']:
-                        _slot_id = None
-                        _account = False
+                        _slot_id: int | None = None
+                        _account: bool = False
                         # Attempt to match the identifier in this prop to any of the known user or account idents
                         if int(prop['identifier']) in _account_ident_to_slot_id:
                             _slot_id = _account_ident_to_slot_id[int(prop['identifier'])]
@@ -157,6 +169,7 @@ class DataCollectorV2:
                         # we have a userID or accountID property match
                         # check if we have previously copied a PlayerEphemeralConsts object for this tick, else
                         # create one by deepcopying the base
+                        _details_base: PlayerEphemeralConsts
                         if tick_num in self.slot_details_by_tick_range[_slot_id]:
                             _details_base = self.slot_details_by_tick_range[_slot_id][tick_num]
                         else:
@@ -213,15 +226,16 @@ class ExtractorV2:
         :param message: the demo 'message' type packet
         :return: None
         """
+        entity: dict[str, Any]
         for entity in message['entities']:
             if not self.data_collection.server_classes.is_id_named(entity['server_class'], UsefulServerClasses.Player):
                 continue
 
-            entity_idx = entity['entity_index']
+            entity_idx: int = entity['entity_index']
             player = self.data_collection.get_player_consts(entity_idx, tick_num)
             if player is None:
-                print(self.data_collection.slot_details_by_tick_range)
-                print(f"Tick: {tick_num}, for entity_idx: {entity_idx}")
+                logger.error(f"Player lookup returned None at Tick: {tick_num}, for entity_idx: {entity_idx}")
+                continue
 
             if player.m_iAccountID is None or player.m_iAccountID <= 256:
                 # BOT players in game will have steam ids ranging from 1 to 100
@@ -230,14 +244,15 @@ class ExtractorV2:
                 continue
 
             # if not a BOT player, m_iAccountID values are always the variable component of a SteamID3.
-            steam_id = SteamID(f"[U:1:{player.m_iAccountID}]")
+            steam_id: SteamID = SteamID(f"[U:1:{player.m_iAccountID}]")
 
-            changes = {}
+            changes: dict[str, dict[str, Any]] = {}
+            prop: dict[str, Any]
             for prop in entity['props']:
                 try:
                     # Expected outcome of the named identifiers' dictionary:
                     # var = {'name': <name>, 'parent': <parent name>, 'type': <some type>}
-                    var = self.data_collection.mapped_identifiers[int(prop['identifier'])]
+                    var: dict[str, str | int] = self.data_collection.mapped_identifiers[int(prop['identifier'])]
                     if var['parent'] not in self._relevant_dts:
                         continue
                     if var['parent'] not in changes:
@@ -255,7 +270,7 @@ class ExtractorV2:
                     # m_vecOrigin[2]: <some other other value>
                     #
                     # This is simply to merge it all into one vector
-                    _zVec = str(var['name'])
+                    _zVec: str = str(var['name'])
                     if _zVec.endswith("[2]") and _zVec.split("[")[0] in changes[var['parent']]:
                         changes[var['parent']][_zVec.split("[")[0]]['z'] = prop['value']
                     else:
@@ -267,15 +282,17 @@ class ExtractorV2:
             self._player_prop_changes[tick_num][str(steam_id)] = changes
 
     def _extract_packets(self) -> None:
+        packet: dict[str, Any]
         for packet in self.demo_json_raw:
             if packet['type'] != "Message":
                 continue
 
-            tick_num = int(packet['tick'])
+            tick_num: int = int(packet['tick'])
             self._player_prop_changes[tick_num] = {}
             # Maybe we would like to include this in the dump at some point.
-            local_player = packet['meta']['view_angles']
+            local_player: dict = packet['meta']['view_angles'] #
 
+            message: dict[str, Any]
             for message in packet['messages']:
                 # Analyse 'PacketEntities' message type
                 # All functions called within this loop are state side-affecting,
@@ -287,6 +304,13 @@ class ExtractorV2:
             self,
             demo_json: dict | str | bytes
     ) -> None:
+        """
+        Takes a demo json, as either a string, bytes or already converted python dict object,
+        and extracts the packets to generate a data_collection object (from DataCollectorV2)
+        and a player_prop_changes by tick mapping. Get it with `this.get_changes()`.
+
+        :param demo_json: A python dict, json str or bytes of a json-converted TF2 demo file
+        """
         self.data_collection = DataCollectorV2(demo_json)
         self.demo_json_raw = self.data_collection.demo_json_raw
         self._player_prop_changes = {}
@@ -294,19 +318,16 @@ class ExtractorV2:
 
         self._extract_packets()
 
-    def get_changes(self) -> dict:
+    def get_changes(self) -> dict[int, dict[str | SteamID, dict]]:
         return self._player_prop_changes
 
 
 def test_v2():
-    from .extractors import Sequencer
-    # _inst = Sequencer(Path("data/demo_trace_2.json"))
-    # _inst.extract_named_identifiers()
-    # _inst.generate_fast_inverse_name_mapping()
-    # _inst.map_ident_value_changes_to_name_times()
-    # _inst.generate_player_ephemeral_consts_mapping()
+    """
+    A small test function for testing V2 capability. won't be in prod
 
-    # print(_inst.get_fast_inverse_name_mapping())
+    :return: None, writes file out to hardcoded file path
+    """
     _v2 = ExtractorV2(Path("data/demo_trace_2.json").read_text('utf8'))
     _changes = _v2.get_changes()
 
